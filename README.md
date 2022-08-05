d291371282db9787b84e46aff611dc7b733c5dea
Felopater Melika <felopatermelika@gmail.com>
2022-08-05 15:39:53 -0500

    type fixes

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ This is just a fun project I built to play around with GraphQL and shopify
 and headless CMS in general.
 
 The way it works is through fetching everything on load through
-getServerSideProps and then passing it to a product component and for
+getStaticProps and then passing it to a product component and for
 Checkout it's a basic mutation function that uses the variant id passed down
 as props and then using next router to push the user to checkout.
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,23 @@ import type {GetStaticProps, NextPage} from 'next'
 import Product from "../components/Product";
 import {gql, GraphQLClient} from "graphql-request";
 
+
 const Home: NextPage = (data) => {
+    // @ts-ignore
     const msg = `Hello Aliens! today is your lucky day! We are running a sale on planets and there is only one left which Is Earth! Get it as quick as you can for only $${data.data.products.edges[0].node.priceRange.minVariantPrice.amount.toString().toUpperCase()}`
-    console.log(data.data?.products.edges[0].node.variants.edges[0].node.id)
     return (
         <div className="w-full flex flex-col justify-center items-center">
             <main className="max-w-4xl p-10 flex flex-col justify-evenly items-center space-y-10 ">
                 <div className="text-2xl text-white text-center font-['jaapokkienchance-regular']">{msg.toUpperCase()}
                 </div>
-                <Product name={data.data?.products.edges[0].node.title.toUpperCase()}
-                         description={data.data?.products.edges[0].node.description.toUpperCase()}
-                         image={data.data?.products.edges[0].node.images.edges[0].node.url}
-                         variantId={data.data?.products.edges[0].node.variants.edges[0].node.id}
+                {/* @ts-ignore */}
+                <Product name={data.data.products.edges[0].node.title.toUpperCase()}
+                    /* @ts-ignore */
+                         description={data.data.products.edges[0].node.description.toUpperCase()}
+                    /* @ts-ignore */
+                         image={data.data.products.edges[0].node.images.edges[0].node.url}
+                    /* @ts-ignore */
+                         variantId={data.data.products.edges[0].node.variants.edges[0].node.id}
                 />
             </main>
         </div>
